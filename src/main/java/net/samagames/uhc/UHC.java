99caaf5dee1de90315cb72200d533c08a2ea9a3e
BlueSlime <jeremylevilain78@orange.fr>
2017-08-24 15:50:27 +0200

    Publishing source code.

--- src/main/java/net/samagames/uhc/UHC.java
+++ src/main/java/net/samagames/uhc/UHC.java
@@ -16,6 +16,22 @@ import net.samagames.survivalapi.modules.gameplay.LoveMachineModule;
 import net.samagames.survivalapi.modules.gameplay.PersonalBlocksModule;
 import org.bukkit.plugin.java.JavaPlugin;
 
+/*
+ * This file is part of UHC.
+ *
+ * UHC is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * UHC is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with UHC.  If not, see <http://www.gnu.org/licenses/>.
+ */
 public class UHC extends JavaPlugin
 {
     @Override
--- src/main/java/net/samagames/uhc/UHCStatisticsHelper.java
+++ src/main/java/net/samagames/uhc/UHCStatisticsHelper.java
@@ -5,13 +5,21 @@ import net.samagames.survivalapi.game.SurvivalGameStatisticsHelper;
 
 import java.util.UUID;
 
-/**
- *                )\._.,--....,'``.
- * .b--.        /;   _.. \   _\  (`._ ,.
- * `=,-,-'~~~   `----(,_..'--(,_..'`-.;.'
+/*
+ * This file is part of UHC.
  *
- * This file is issued of the project UHC
- * Created by Jérémy L. (BlueSlime) on 29/07/16
+ * UHC is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * UHC is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with UHC.  If not, see <http://www.gnu.org/licenses/>.
  */
 public class UHCStatisticsHelper implements SurvivalGameStatisticsHelper
 {
